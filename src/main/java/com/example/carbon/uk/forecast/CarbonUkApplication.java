9e1930b616b294e3f00783fceb1f16f2d560af8f
Alekro <oleksiikoleshyn@smartoakprojects.com>
2026-07-02 20:28:21 +0200

    init commit

---
+++ src/main/java/com/example/carbon/uk/forecast/CarbonUkApplication.java
@@ -0,0 +1,13 @@
+package com.example.carbon.uk.forecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonUkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonUkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carbon/uk/forecast/CarbonUkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carbon.uk.forecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonUkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
